21f9352901f8990552e1357ab5039978575e7a25
Haqiqi Oktaviani <72081534+haqiqiokta05@users.noreply.github.com>
2020-11-20 14:20:48 +0700

    Add files via upload

---
+++ Kelompok 10 PP.py
@@ -0,0 +1,56 @@
+import psutil as ps 
+import time 
+import sys 
+import os 
+import subprocess 
+
+def get_monitoring(arg): 
+	ipadd = getip_by_iface(arg);  
+	
+	print('\ n') 
+	print('IP Address:' + ipadd) 
+
+	cpu = ps.cpu_percent() 
+	ram = ps.virtual_memory().percent 
+	print('CPU Used: {}\nRAM Used :{}' .format(cpu, ram)) 
+	rx_bytes, tx_bytes = get_txrx(arg) 
+	rx_kb = int(rx_bytes)/1000 
+	tx_kb = int(tx_bytes)/1000 
+	print(f"Tx / Rx [eth]: {tx_kb} kbps / {rx_kb} kbps") 
+	time.sleep(2)
+
+def get_txrx(interface): 
+	for line in open('/ proc / net / dev', 'r'): 
+		if interface in line: 
+			data = line.split('% s:  '% interface)[1].split() 
+			rx_bytes, tx_bytes = (data[0], data[8]) 
+			return (rx_bytes, tx_bytes)
+
+def getip_by_iface(iface): 
+	return os.popen("ip addr i grep" + iface +  "i awk '{print $ 2}' I tail -n +2"). read() .strip(); 
+
+def getalllinterface(): 
+	return subprocess.check_output("nmcli di awk '{print $ 1}' I tail -n  +2 i head -n -1 ", shell = True, universal_newlines = True);
+
+def getip_by_iface(iface): 
+	return os.popen("ip addr i grep" + iface + "i awk '(print $ 2)' i tail -n +2").read().strip(); 
+
+def getallliterface():  
+	return subprocess.check_output("nmcli di awk '{print $ 13' i tail -n +2: head -n -1", shell = True, universal_newlines = True); 
+
+if __name__ == " __main__": 
+	os. system ("clear") 
+	print("Select Node:") 
+	iface = getalllinterface().split('\ n') 
+	iface.remove("") 
+	for idx, iface in enumerate(iface): 
+		if(iface != ''):  
+			print("{0}. Node{0}".format(idx + 1)); 
+
+	print("\nMasukan Perintah/Pilihan Anda:"); 
+	opti = int (input ()); 
+	if (opti <1) or  (opti> len (iface)): 
+		print('Perintah tidak dikenal') 
+	else: 
+		get_monitoring (iface[opti-1]) 
+		sys.exit (1);
